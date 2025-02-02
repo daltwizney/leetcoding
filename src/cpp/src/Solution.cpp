8176ffff81dbb0eec9e516f52c6ad0653de0b8a3
danrae pray <danrae.pray@outlook.com>
2025-02-01 19:09:12 -0600

    moar leetcoding!

--- src/cpp/src/Solution.cpp
+++ src/cpp/src/Solution.cpp
@@ -1,22 +1,76 @@
 #include <unordered_set>
 
+#include <unordered_map>
+#include <queue>
+
 using namespace std;
 
 class Solution {
 public:
-    bool checkIfPangram(string sentence) {
 
-        unordered_set<char> seen;
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+        if (n < 1) {
+            return false;
+        }
+        else if (source == destination) {
+            return true;
+        }
 
-        const char* characters = sentence.c_str();
+        // build adjacency list
+        unordered_map<int, vector<int>> graph;
 
-        for (int i = 0; i < sentence.length(); ++i)
+        for (int i = 0; i < edges.size(); ++i)
         {
-            seen.emplace(characters[i]);
+            vector<int> edge = edges[i];
 
-            if (seen.size() == 26)
+            int a = edge[0];
+            int b = edge[1];
+
+            if (graph.find(a) == graph.end())
+            {
+                graph[a] = vector<int>();
+            }
+
+            if (graph.find(b) == graph.end())
+            {
+                graph[b] = vector<int>();
+            }
+
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+
+        // search for path
+        queue<int> buffer;
+
+        unordered_set<int> visited;
+
+        buffer.emplace(source);
+
+        while (buffer.size() > 0)
+        {
+            int node = buffer.front();
+            buffer.pop();
+
+            if (node == destination)
             {
                 return true;
+            }
+
+            visited.emplace(node);
+
+            vector<int> neighbors = graph[node];
+
+            for (int i = 0; i < neighbors.size(); ++i)
+            {
+                int neighbor = neighbors[i];
+
+                if (visited.find(neighbor) == visited.end())
+                {
+                    visited.emplace(neighbor);
+                    buffer.emplace(neighbor);
+                }
             }
         }
 
